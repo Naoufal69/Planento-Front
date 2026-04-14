09abc084662336b183ca41d4c905c7392328b816
Naoufal69 <naoufal.ayache@ecole-isitech.fr>
2026-04-14 15:12:28 +0200

    #4 - Route de chantier

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import IndexView from '../view/IndexView.vue'
+import IndexView from '@/views/IndexView.vue'
+import InProgressView from '@/views/InProgressView.vue'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -7,11 +8,33 @@ const routes: RouteRecordRaw[] = [
     name: 'home',
     component: IndexView,
   },
+  {
+    path: '/in-progress',
+    name: 'in-progress',
+    component: InProgressView,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+const isInProgress = import.meta.env.VITE_APP_IN_PROGRESS === 'true'
+
+router.beforeEach((to) => {
+  if (isInProgress) {
+    if (to.name === 'in-progress') {
+      return true
+    }
+    return { name: 'in-progress' }
+  }
+
+  if (to.name === 'in-progress') {
+    return { name: 'home' }
+  }
+
+  return true
 })
 
 export default router
